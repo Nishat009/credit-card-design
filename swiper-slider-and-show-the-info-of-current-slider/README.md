22841b256c0db231125feba2ac1a91adcdc3b998
Prantik0616 <prantik.techvill@gmail.com>
2024-09-03 11:55:29 +0600

    credit card design

---
+++ swiper-slider-and-show-the-info-of-current-slider/README.md
@@ -0,0 +1,4 @@
+# swiper slider and show the info of current slider
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Prantik1606/pen/zYVamOd](https://codepen.io/Prantik1606/pen/zYVamOd).
+
---
+++ swiper-slider-and-show-the-info-of-current-slider/src/script.js
@@ -0,0 +1,21 @@
+   const swiper = new Swiper(".swiper", {
+            slidesPerView: 1.2,
+            spaceBetween: 32,
+            freeMode: {
+                enabled: true,
+                sticky: true,
+                momentum: false
+            },
+            on: {
+                slideChange: function() {
+                    var currentSlide = swiper.slides[swiper.activeIndex];
+                    var slideContent = currentSlide.innerHTML;
+                    document.getElementById('slide-info').innerHTML = slideContent;
+                }
+            }
+        });
+        document.addEventListener('DOMContentLoaded', function() {
+            var initialSlide = swiper.slides[swiper.activeIndex];
+            var initialContent = initialSlide.innerHTML;
+            document.getElementById('slide-info').innerHTML = initialContent;
+        });
